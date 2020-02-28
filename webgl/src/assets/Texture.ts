3638bc40557d6c4bb06d8a6b141cca55ea34dcbf
blitz.whtoo <whtoo@qq.com>
2020-02-28 10:16:49 +0800

    1. 增加 message定义

---
+++ webgl/src/assets/Texture.ts
@@ -0,0 +1,9 @@
+namespace TSE {
+    const LEVEL : number = 0
+    const BORDER : number = 0
+    const TEMP_IMAGE_DATA: Uint8Array = new Uint8Array([255,255,255,255])
+
+    export class Texture implements IMessageHandler {
+        
+    }
+}
